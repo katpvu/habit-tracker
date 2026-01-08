94f0589ea59e59e7ead9b244020ae1d1a2ebd2e9
Kat Vu <vu.kathy98@gmail.com>
2026-01-08 00:03:46 -0800

    add logging

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
+import logging
 from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse
 from .core.exceptions import AppException
 from .dependencies.auth import get_current_user
 from .models import User
 from .routers import habit_cycles, habits
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
 
 # Can declare global dependencies that will be combined with deps for each API Router
 app = FastAPI(
